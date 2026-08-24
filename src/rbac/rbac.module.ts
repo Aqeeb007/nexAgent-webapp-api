ec668947dc75bdb85aefc65a81dac009888805f0
Aqeeb007 <aquebmohammad@gmail.com>
2026-08-24 12:33:49 +0530

    refactor(rbac, users): remove unused RBAC test controller and user DTOs

--- src/rbac/rbac.module.ts
+++ src/rbac/rbac.module.ts
@@ -3,11 +3,9 @@ import { Module } from '@nestjs/common';
 import { RbacService } from './rbac.service';
 import { RolesService } from './roles.service';
 import { PermissionGuard } from './guards/permission.guard';
-import { RbacTestController } from './rbac-test.controller';
 
 @Module({
   providers: [RbacService, RolesService, PermissionGuard],
   exports: [RbacService, RolesService, PermissionGuard],
-  controllers: [RbacTestController],
 })
 export class RbacModule {}
